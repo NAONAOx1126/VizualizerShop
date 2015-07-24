e5925f9e1735b6ca2e7b6a730f2203db85b0f7fb
Naohisa Minagawa <info@clay-system.jp>
2015-07-24 11:03:45 +0900

    #1.1.1 注文時や定期購入の決済時にメールを送信するように修正

--- classes/VizualizerShop/Model/OrderShip.php
+++ classes/VizualizerShop/Model/OrderShip.php
@@ -60,4 +60,64 @@ class VizualizerShop_Model_OrderShip extends Vizualizer_Plugin_Model
         $model = $loader->loadModel("OrderDetail");
         return $model->findAllBy(array("order_ship_id" => $this->order_ship_id));
     }
+
+    /**
+     * 注文情報を取得する
+     */
+    public function order(){
+        $loader = new Vizualizer_Plugin("shop");
+        $model = $loader->loadModel("Order");
+        $model->findByPrimaryKey($this->order_id);
+        return $model;
+    }
+
+    /**
+     * データを登録する。
+     */
+    public function save($ignoreOperator = false)
+    {
+        $shipped = false;
+        if (array_key_exists("shipment_status", $this->values_org) && array_key_exists("shipment_status", $this->values)) {
+            if ($this->values_org["shipment_status"] != 2 && $this->values_org["shipment_status"] != 3 && $this->values["shipment_status"] == 2) {
+                $shipped = true;
+            }
+        }
+        parent::save($ignoreOperator);
+        if($shipped && Vizualizer_Configure::exists("shipped_mail_title") && Vizualizer_Configure::exists("shipped_mail_template")){
+            // メールの内容を作成
+            $title = Vizualizer_Configure::get("shipped_mail_title");
+            $templateName = Vizualizer_Configure::get("shipped_mail_template");
+            $attr = Vizualizer::attr();
+            $template = $attr["template"];
+            if(!empty($template)){
+                $body = $template->fetch($templateName.".txt");
+
+                // ショップの情報を取得
+                $loader = new Vizualizer_Plugin("admin");
+                $company = $loader->loadModel("Company");
+                $order = $this->order();
+                if($order->isLimitedCompany() && $order->limitCompanyId() > 0){
+                    $company->findByPrimaryKey($order->limitCompanyId());
+                }else{
+                    $company->findBy(array());
+                }
+
+                // 購入者にメール送信
+                $mail = new Vizualizer_Sendmail();
+                $mail->setFrom($company->email);
+                $mail->setTo($order->order_email);
+                $mail->setSubject($title);
+                $mail->addBody($body);
+                $mail->send();
+
+                // ショップにメール送信
+                $mail = new Vizualizer_Sendmail();
+                $mail->setFrom($order->order_email);
+                $mail->setTo($company->email);
+                $mail->setSubject($title);
+                $mail->addBody($body);
+                $mail->send();
+            }
+        }
+    }
 }
