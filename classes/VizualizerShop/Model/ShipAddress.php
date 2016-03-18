9bbb7aadcabf9d4444382ee4c63c1f58702bde26
Naohisa Minagawa <info@clay-system.jp>
2016-03-18 20:24:30 +0900

    #1.1.7 配送料のテーブルを変更

---
+++ classes/VizualizerShop/Model/ShipAddress.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Copyright (C) 2012 Vizualizer All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author    Naohisa Minagawa <info@vizualizer.jp>
+ * @copyright Copyright (c) 2010, Vizualizer
+ * @license http://www.apache.org/licenses/LICENSE-2.0.html Apache License, Version 2.0
+ * @since PHP 5.3
+ * @version   1.0.0
+ */
+
+/**
+ * 宛先別配送のモデルです。
+ *
+ * @package VizualizerShop
+ * @author Naohisa Minagawa <info@vizualizer.jp>
+ */
+class VizualizerShop_Model_ShipAddress extends VizualizerShop_Model_MallModel
+{
+    /**
+     * コンストラクタ
+     *
+     * @param $values モデルに初期設定する値
+     */
+    public function __construct($values = array())
+    {
+        $loader = new Vizualizer_Plugin("shop");
+        parent::__construct($loader->loadTable("ShipAddresses"), $values);
+    }
+
+    /**
+     * 主キーでデータを取得する。
+     *
+     * @param $ship_address_id 重量配送ID
+     */
+    public function findByPrimaryKey($ship_address_id)
+    {
+        $this->findBy(array("ship_address_id" => $ship_address_id));
+    }
+}
